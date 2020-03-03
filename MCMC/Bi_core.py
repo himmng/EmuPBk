1dedbbc01dbef894e6e2adb9b054ba98970a8a22
himmng <himanshuhimang@gmail.com>
2020-03-03 12:06:35 +0530

    Edited README.md.

---
+++ MCMC/Bi_core.py
@@ -0,0 +1,29 @@
+import numpy as np
+from ANN.BkEmu.Bipectrum_Emulator import Bk_pred
+
+#======================================================================================================================#
+
+class PScore(object):
+
+    def __init__(self):
+        pass                                    #=====================CosmoHammer core module for THANN================#
+    def __call__(self, ctx):
+        params = ctx.getParams()
+
+        ctx.add('params_pk',params)
+        n_ion, R_mfp, NoH = params
+        prm = np.array([[n_ion, R_mfp, NoH]])
+        prm.reshape(1,3)
+
+        Bk_th = Bk_pred(prm)
+
+        ctx.add("key_data",Bk_th)
+
+    def setup(self):
+
+        print("Bispectrum core setup is done")
+
+#======================================================================================================================#
+
+
+
--- data/data_Bispectrum/data_read.py
+++ data/data_Bispectrum/data_read.py
@@ -1,5 +1,30 @@
 import numpy as np
 
+path = '/home/ht/EmuPBk/data/data_Bispectrum/k0.2/'
+
+N_bins = np.loadtxt('nbins02')
+
+Bk = np.loadtxt('bk_norm02')
+
+params = np.loadtxt('params02')
+
+sigma = Bk/np.sqrt(N_bins)
+cov_inv = 1./sigma
+
+cov = np.zeros(shape=(1058,550,550))
+for i in range(1058):
+    for j in range(550):
+        cov[i][j][j] = cov_inv[i][j]
+
+ind = np.loadtxt('index')
+index = np.zeros(len(ind),dtype=int)
+for i in range(len(ind)):
+    index[i] = int(ind[i])
+
+Bk_test = Bk[index]
+params_test = params[index]
+cov_inv_test = cov[index]
+
 
 
 
