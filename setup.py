45c580d7d64dd7ec51ba099494dc8e644bce58b3
himmng <himanshuhimang@gmail.com>
2020-04-27 17:01:31 +0530

    updated version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.rst", "r") as fh:
 
 setuptools.setup(
     name="EmuPBk",
-    version='2.1.0',
+    version='2.0',
     author='Himanshu Tiwari',
     author_email="himanshuhimang@gmail.com",
     packages=setuptools.find_packages(),
